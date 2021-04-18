964121715ec32929290bbbf9d5fd0395c7b72b4d
Dasha <walter.dasha@gmail.com>
2021-04-18 11:52:04 +0200

    Rename webapp to people to separate sections of the site

--- heritage/heritage/settings.py
+++ heritage/heritage/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webapp.apps.WebappConfig',
+    'people.apps.PeopleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
