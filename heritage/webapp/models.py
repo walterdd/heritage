f89e73a8733237658eb611a9f562f573eb179290
Dasha <walter.dasha@gmail.com>
2021-04-18 11:32:24 +0200

    Add Text and Tag models

--- heritage/webapp/models.py
+++ heritage/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+  name = models.CharField(max_length=200, primary_key=True)
+  publication_date = models.DateTimeField(auto_now_add=True)
+
+
+class Text(models.Model):
+  text = models.TextField()
+  tags = models.ManyToManyField(Tag)
